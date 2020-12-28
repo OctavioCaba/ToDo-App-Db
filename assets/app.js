af7958aa6bf1333dea4d9f7301246c9011126662
Octavio Caba <cabaoctavio@hotmail.com>
2020-12-27 21:01:05 -0300

    v1.1 - Casi terminado

---
+++ assets/app.js
@@ -0,0 +1,43 @@
+const createTaskBtn = document.getElementById('create-task-btn');
+const completeTaskBtn = document.querySelectorAll('.complete-task-btn');
+const deleteTaskBtn = document.querySelectorAll('.delete-task-img');
+const list = document.getElementById('list');
+const themeSwitch = document.getElementById('theme');
+
+for (let i = 0; i < completeTaskBtn.length; i++) {
+    completeTaskBtn[i].addEventListener('click', () => {
+        if (!completeTaskBtn[i].classList.contains('active')) {
+            completeTaskBtn[i].classList.add('active');
+            completeTaskBtn[i].firstElementChild.style.visibility = 'visible';
+        } else {
+            completeTaskBtn[i].classList.remove('active');
+            completeTaskBtn[i].firstElementChild.style.visibility = 'hidden';
+        }
+    });
+}
+
+for (let i = 0; i < deleteTaskBtn.length; i++) {
+    deleteTaskBtn[i].addEventListener('click', () => {
+        console.log(deleteTaskBtn[i]);
+    });
+}
+
+createTaskBtn.addEventListener('click', () => {
+    if (document.getElementById('input-task').value != '') {
+        let taskTitle = document.getElementById('input-task').value;
+        alert('Task \"' + taskTitle + '\" created');
+        document.getElementById('input-task').value = '';
+    } else {
+        alert('Task\'s title can\'t be empty');
+    }
+    completeTaskBtn = document.querySelectorAll('.complete-task-btn');
+    deleteTaskBtn = document.querySelectorAll('.delete-task-img');
+});
+
+themeSwitch.addEventListener('click', () => {
+    if (!document.body.attributes['data-theme']) {
+        document.body.setAttribute('data-theme', 'dark');
+    } else {
+        document.body.removeAttribute('data-theme');
+    }
+});
